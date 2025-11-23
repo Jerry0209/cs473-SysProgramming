220901dfe1548e58e330346f58bc86978059f0d1
Jerry0209 <mr.htianrui@gmail.com>
2025-11-23 16:48:47 +0100

    PW 5 Finished with Wierd IRQ Latnecy Counter

--- virtualprototype/programs/support/include/swap.h
+++ virtualprototype/programs/support/include/swap.h
@@ -7,6 +7,13 @@
 extern "C" {
 #endif
 
+/** Byte swap a 32-bit unsigned integer 
+ *  The swap_u32 function performs a 32-bit byte swap (endianness conversion). 
+ *  It uses inline assembly to call a NIOS II custom instruction (l.nios_rrr with opcode 0x1) that reverses the byte order of the input value. 
+ *  For example, 0x12345678 becomes 0x78563412.
+ **/
+
+
 __static_inline uint32_t swap_u32(uint32_t src) {
     uint32_t dest;
     asm volatile("l.nios_rrr %[out1],%[in1],r0,0x1" : [out1] "=r"(dest) : [in1] "r"(src));
